80a7332ce2d757c17e154c676cd26b7942452c9b
yas3jb <yasmu213@gmail.com>
2024-03-15 21:47:17 -0400

    created db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const pg = require("pg");
 const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_talent_agency_db"
+  process.env.DATABASE_URL || "postgres://localhost/the_acme_store"
 );
 const uuid = require("uuid");
 const bcrypt = require("bcrypt");
