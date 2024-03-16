3006a20a307d1cf59245a1e9e8cc88ff1558e904
yas3jb <yasmu213@gmail.com>
2024-03-15 21:13:01 -0400

    created users and products, logged IDs.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const { client, createTables } = require("./db");
+const { client, createTables, createUser, createProduct } = require("./db");
 
 const init = async () => {
   await client.connect();
   console.log("connected to database");
   await createTables();
   console.log("tables created");
+  const [moe, lucy, ethyl, tablet, laptop, smartphone, smartwatch] =
+    await Promise.all([
+      createUser({ username: "moe", password: "s3cr3t" }),
+      createUser({ username: "lucy", password: "s3cr3t!!" }),
+      createUser({ username: "ethyl", password: "shhh" }),
+      createProduct({ name: "laptop" }),
+      createProduct({ name: "smartphone" }),
+      createProduct({ name: "smartwatch" }),
+      createProduct({ name: "tablet" }),
+    ]);
+  console.log(moe.id);
+  console.log(laptop.id);
 };
 init();
