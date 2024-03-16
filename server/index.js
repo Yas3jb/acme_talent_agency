9a2cb12b287085eba5f6bb4e0cdd771a783dae8c
yas3jb <yasmu213@gmail.com>
2024-03-15 21:04:45 -0400

    add an init function

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const { client, createTables } = require("./db");
+
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+  await createTables();
+  console.log("tables created");
+};
+init();
